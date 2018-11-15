5f6c5448764ac5eff3a9e105c42289132c021c4e
siddharthpathak <34261337+siddharthpk@users.noreply.github.com>
2018-11-15 14:06:52 -0800

    Updated code

--- MazeSolver.java
+++ MazeSolver.java
@@ -3,7 +3,7 @@
 * ID: V00876495
 * Date: MARCH 12th, 2017
 * Filename: MazeSolver.java
-* Details: CSC 115 Assignment 3
+* 
 */
 
 
@@ -16,11 +16,10 @@
  *   class that contains a single public static method called
  *   "findPath". To involve the method one must have already created
  *   an instance of Maze. The method must return a path through the
- *   maze (if it exists) in the format shown within the Assignment #3
+ *   maze (if it exists) in the format shown within the
  *   description.
  *
- * Note: You are free to add to this class whatever other methods will
- * help you in writing a solution to A#3 part 2.
+ * 
  */
 
 public class MazeSolver {
